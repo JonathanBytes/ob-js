17c7bf9dc39c1d1f86eea548966bdbebb750d3bd
JonathanWin <jonathancardonaramirez@gmail.com>
2023-05-01 19:33:53 -0500

    Solve second exercise

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
+const list = [
+  'Jonathan', // Name
+  22, // Age
+  false, // Developer
+  new Date(2000,8,19,11,23,11), // Birth date
+  {
+    title : 'La isla misteriosa',
+    author : 'Julio Verne',
+    date : "1875",
+    link : 'https://es.wikipedia.org/wiki/La_isla_misteriosa',
+  },
+]
+
+console.log(list[list.length - 1].link)
